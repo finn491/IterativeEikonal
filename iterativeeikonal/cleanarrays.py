cdad4fdbf564812a9afecce5328bdfd7ee1e2118
Finn Sherry <f.m.sherry@tue.nl>
2023-11-28 10:54:44 +0100

    improve visualisation of image arrays.

--- iterativeeikonal/cleanarrays.py
+++ iterativeeikonal/cleanarrays.py
@@ -3,6 +3,7 @@
 import taichi as ti
 import numpy as np
 from PIL import Image
+import matplotlib.pyplot as plt
 
 # NumPy Arrays
 
@@ -34,9 +35,36 @@ def unpad_array(u, pad_shape=1):
     centre_slice = extract_centre_slice(u, pad_shape=pad_shape)
     return u[centre_slice]
 
+def convert_array_to_image(image_array):
+    """Convert numpy array `image_array` to a grayscale PIL Image object."""
+    if image_array.dtype == "uint8":
+        image = Image.fromarray(image_array, mode="L")
+    else:
+        image = Image.fromarray((image_array * 255).astype("uint8"), mode="L")
+    return image
+
 def view_image_array(image_array):
     """View numpy array `image_array` as a grayscale image."""
-    return Image.fromarray((image_array * 255).astype("uint8"), mode="L")
+    image = convert_array_to_image(image_array)
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(5, 5))
+    ax.imshow(image, cmap="gray")
+    ax.set_axis_off()
+    return image, fig
+
+def view_image_arrays_side_by_side(image_array_list):
+    """
+    View list of numpy array `image_array_list` side by side as grayscale 
+    images.
+    """
+    image_list = []
+    ncols = len(image_array_list)
+    fig, ax = plt.subplots(nrows=1, ncols=ncols, figsize=(5 * ncols, 5))
+    for i, image_array in enumerate(image_array_list):
+        image = convert_array_to_image(image_array)
+        image_list.append(image)
+        ax[i].imshow(image_array, cmap="gray")
+        ax[i].set_axis_off()
+    return image_list, fig
 
 # TaiChi Fields
 
