70be07e6ba82cdc4f02d83f8a2cb2de7766e37e3
Finn Sherry <f.m.sherry@tue.nl>
2023-12-18 16:44:08 +0100

    visualise isosurfaces in SE(2) using marching cubes.

--- iterativeeikonal/__init__.py
+++ iterativeeikonal/__init__.py
@@ -6,5 +6,8 @@ import iterativeeikonal.costfunctions as costfunctions
 import iterativeeikonal.solvers as solvers
 
 # Most important functions are available at top level
+## R2
 from iterativeeikonal.solvers import eikonal_solver_R2, geodesic_back_tracking_R2, convert_continuous_indices_to_real_space_R2
 from iterativeeikonal.costfunctions import multiscale_frangi_filter_R2, cost_function
+## SE2
+from iterativeeikonal.solvers import eikonal_solver_SE2_LI, geodesic_back_tracking_SE2
