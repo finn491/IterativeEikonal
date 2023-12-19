1f2610ac482df7c9227a35e53fd68630e3821c69
Finn Sherry <f.m.sherry@tue.nl>
2023-12-19 14:56:32 +0100

    left invariant solvers now work with general (nondiagonal) metric tensors.

--- iterativeeikonal/solvers.py
+++ iterativeeikonal/solvers.py
@@ -206,7 +206,7 @@ def distance_gradient_field_R2(
 
 # SE(2)
 
-def eikonal_solver_SE2_LI(G_np, cost_np, source_point, n_max=1e5):
+def eikonal_solver_SE2_LI(G_inv_np, cost_np, source_point, dxy, n_max=1e5):
     """
     Solve the Eikonal PDE on SE(2) equipped with a datadriven left invariant 
     metric tensor field defined by `metric_tensor_diagonal` and `cost_np`, with 
@@ -215,13 +215,12 @@ def eikonal_solver_SE2_LI(G_np, cost_np, source_point, n_max=1e5):
     SE(2)" (2015).
 
     Args:
-        `G_np`: np.ndarray(shape=(3,), dtype=[float]) of constants of diagonal 
-          metric tensor with respect to left invariant basis.
+        `G_inv_np`: np.ndarray(shape=(3, 3), dtype=[float]) of constants of the 
+          inverse metric tensor with respect to left invariant basis.
         `cost_np`: np.ndarray of cost function.
         `source_point`: Tuple[int] describing index of source point in 
           `cost_np`.
-        `target_point`: Tuple[int] describing index of target point in 
-          `cost_np`.
+        `dxy`: Spatial step size, taking values greater than 0.
       Optional:
         `n_max`: Maximum number of iterations, taking positive values. Defaults 
           to 1e5.
@@ -233,10 +232,10 @@ def eikonal_solver_SE2_LI(G_np, cost_np, source_point, n_max=1e5):
           respect to cost function described by `cost_np`.
     """
     shape = cost_np.shape
-    ε = cost_np.min()
+    ε = cost_np.min() * dxy
     cost = get_padded_cost(cost_np)
     W = get_initial_W(shape)
-    G = ti.Vector(G_np, ti.f32)
+    G_inv = ti.Matrix(G_inv_np, ti.f32)
 
     # Create empty Taichi objects
     A1_forward = ti.field(dtype=ti.f32, shape=W.shape)
@@ -245,48 +244,45 @@ def eikonal_solver_SE2_LI(G_np, cost_np, source_point, n_max=1e5):
     A2_backward = ti.field(dtype=ti.f32, shape=W.shape)
     A3_forward = ti.field(dtype=ti.f32, shape=W.shape)
     A3_backward = ti.field(dtype=ti.f32, shape=W.shape)
-    abs_A1 = ti.field(dtype=ti.f32, shape=W.shape)
-    abs_A2 = ti.field(dtype=ti.f32, shape=W.shape)
-    abs_A3 = ti.field(dtype=ti.f32, shape=W.shape)
-    dW_dt = ti.field(dtype=ti.f32, shape=W.shape)
     A1_W = ti.field(dtype=ti.f32, shape=W.shape)
     A2_W = ti.field(dtype=ti.f32, shape=W.shape)
     A3_W = ti.field(dtype=ti.f32, shape=W.shape)
+    dW_dt = ti.field(dtype=ti.f32, shape=W.shape)
     grad_W = ti.Vector.field(n=3, dtype=ti.f32, shape=W.shape)
     
-    boundarypoints, boundaryvalues = get_boundary_conditions_R2(source_point)
+    boundarypoints, boundaryvalues = get_boundary_conditions_SE2(source_point)
     eik.cleanarrays.apply_boundary_conditions(W, boundarypoints, boundaryvalues)
 
     # Compute approximate distance map
     for _ in tqdm(range(int(n_max))):
-        step_W_SE2_LI(W, cost, G, A1_forward, A1_backward, A2_forward, A2_backward, A3_forward, A3_backward, abs_A1, 
-                      abs_A2, abs_A3, ε, dW_dt)
+        step_W_SE2_LI(W, cost, G_inv, A1_forward, A1_backward, A2_forward, A2_backward, A3_forward, A3_backward, A1_W, 
+                      A2_W, A3_W, dxy, ε, dW_dt)
         eik.cleanarrays.apply_boundary_conditions(W, boundarypoints, boundaryvalues)
-    # print(f"Converged after {n - 1} steps!")
 
     # Compute gradient field: note that ||grad_cost W|| = 1 by Eikonal PDE.
-    distance_gradient_field_SE2_LI(W, cost, A1_forward, A1_backward, A2_forward, A2_backward, A3_forward, A3_backward,
-                                   A1_W, A2_W, A3_W, grad_W)
+    distance_gradient_field_SE2_LI(W, cost, G_inv, dxy, A1_forward, A1_backward, A2_forward, A2_backward, A3_forward, 
+                                   A3_backward, A1_W, A2_W, A3_W, grad_W)
 
     # Cleanup
     W_np = W.to_numpy()
     grad_W_np = grad_W.to_numpy()
-    return eik.cleanarrays.unpad_array(W_np), eik.cleanarrays.unpad_array(grad_W_np, pad_shape=(1, 1, 0))
+    return eik.cleanarrays.unpad_array(W_np), eik.cleanarrays.unpad_array(grad_W_np) # , pad_shape=(1, 1, 0))
 
 @ti.kernel
 def step_W_SE2_LI(
     W: ti.template(),
     cost: ti.template(),
-    G: ti.types.vector(3, ti.f32),
+    G_inv: ti.types.matrix(3, 3, ti.f32),
     A1_forward: ti.template(),
     A1_backward: ti.template(),
     A2_forward: ti.template(),
     A2_backward: ti.template(),
     A3_forward: ti.template(),
     A3_backward: ti.template(),
-    abs_A1: ti.template(),
-    abs_A2: ti.template(),
-    abs_A3: ti.template(),
+    A1_W: ti.template(),
+    A2_W: ti.template(),
+    A3_W: ti.template(),
+    dxy: ti.f32,
     ε: ti.f32,
     dW_dt: ti.template()
 ):
@@ -300,31 +296,43 @@ def step_W_SE2_LI(
     Args:
       Static:
         `cost`: ti.field(dtype=[float], shape=shape) of cost function.
-        `G`: ti.types.vector(n=3, dtype=[float]) of constants of diagonal 
-          metric tensor with respect to left invariant basis. 
-        `A*_*`: ti.field(dtype=[float], shape=shape) of derivatives.
+        `G_inv`: ti.types.matrix(n=3, m=3, dtype=[float]) of constants of 
+          inverse metric tensor with respect to left invariant basis.
+        `dxy`: Spatial step size, taking values greater than 0.
         `ε`: "Time" step size, taking values greater than 0.
         `*_target`: Indices of the target point.
       Mutated:
         `W`: ti.field(dtype=[float], shape=shape) of approximate distance map, 
           which is updated in place.
+        `A*_*`: ti.field(dtype=[float], shape=shape) of derivatives.
+        `A*_W`: ti.field(dtype=[float], shape=shape) of upwind derivative of the 
+          approximate distance map in the A* direction, which is updated in 
+          place.
         `dW_dt`: ti.field(dtype=[float], shape=shape) of error of the distance 
           map with respect to the Eikonal PDE, which is updated in place.
-        `abs_A*`: ti.field(dtype=[float], shape=shape) of absolute values of
-          derivatives, which are updated in place.
     """
-    eik.derivativesSe2.abs_derivatives(W, 1., A1_forward, A1_backward, A2_forward, A2_backward, A3_forward, A3_backward,
-                                       abs_A1, abs_A2, abs_A3)
+    eik.derivativesSE2.derivatives_LI(W, dxy, A1_forward, A1_backward, A2_forward, A2_backward, A3_forward, A3_backward)
+    eik.derivativesSE2.upwind_derivatives_LI(W, dxy, A1_forward, A1_backward, A2_forward, A2_backward, A3_forward,
+                                             A3_backward, A1_W, A2_W, A3_W)
     for I in ti.grouped(W):
         # It seems like TaiChi does not allow negative exponents.
-        dW_dt[I] = 1 - (ti.math.sqrt((abs_A1[I]**2 / G[0] + abs_A2[I]**2 / G[1] + abs_A3[I]**2 / G[2])) / cost[I])
+        dW_dt[I] = 1 - (ti.math.sqrt(
+            G_inv[0, 0] * A1_W[I]**2 +
+            2 * G_inv[0, 1] * A1_W[I] * A2_W[I] + # Metric tensor is symmetric.
+            2 * G_inv[0, 2] * A1_W[I] * A3_W[I] +
+            G_inv[1, 1] * A2_W[I]**2 +
+            2 * G_inv[1, 2] * A2_W[I] * A3_W[I] +
+            G_inv[2, 2] * A3_W[I]**2
+        ) / cost[I])
+        # dW_dt[I] = 1 - (ti.math.sqrt((abs_A1[I]**2 / G[0] + abs_A2[I]**2 / G[1] + abs_A3[I]**2 / G[2])) / cost[I])
         W[I] += dW_dt[I] * ε
 
 @ti.kernel
 def distance_gradient_field_SE2_LI(
     W: ti.template(),
     cost: ti.template(),
-    G: ti.types.vector(3, ti.f32),
+    G_inv: ti.types.matrix(3, 3, ti.f32),
+    dxy: ti.f32,
     A1_forward: ti.template(),
     A1_backward: ti.template(),
     A2_forward: ti.template(),
@@ -346,29 +354,27 @@ def distance_gradient_field_SE2_LI(
       Static:
         `W`: ti.field(dtype=[float], shape=shape) of approximate distance map.
         `cost`: ti.field(dtype=[float], shape=shape) of cost function.
-        `G`: ti.types.vector(n=3, dtype=[float]) of constants of diagonal 
-          metric tensor with respect to left invariant basis. 
+        `G_inv`: ti.types.matrix(n=3, m=3, dtype=[float]) of constants of 
+          diagonal metric tensor with respect to left invariant basis.
+        `dxy`: Spatial step size, taking values greater than 0.
       Mutated:
         `A*_*`: ti.field(dtype=[float], shape=shape) of derivatives, which are 
           updated in place.
-        `A1_W`: ti.field(dtype=[float], shape=shape) of upwind derivative of the 
-          approximate distance map in the A1 direction, which is updated in 
-          place.
-        `A2_W`: ti.field(dtype=[float], shape=shape) of upwind derivative of the 
-          approximate distance map in the A2 direction, which is updated in 
-          place.
-        `A3_W`: ti.field(dtype=[float], shape=shape) of upwind derivative of the 
-          approximate distance map in the A3 direction, which is updated in 
+        `A*_W`: ti.field(dtype=[float], shape=shape) of upwind derivative of the 
+          approximate distance map in the A* direction, which is updated in 
           place.
         `grad_W`: ti.field(dtype=[float], shape=shape) of upwind derivatives of 
           approximate distance map, which is updated inplace.
     """
-    dxy = 1.
     eik.derivativesSE2.derivatives_LI(W, dxy, A1_forward, A1_backward, A2_forward, A2_backward, A3_forward, A3_backward)
     eik.derivativesSE2.upwind_derivatives_LI(W, dxy, A1_forward, A1_backward, A2_forward, A2_backward, A3_forward,
                                              A3_backward, A1_W, A2_W, A3_W)
     for I in ti.grouped(A1_W):
-        grad_W[I] = ti.Vector([A1_W[I] / G[0], A2_W[I] / G[1], A3_W[I] / G[2]]) / cost[I]
+        grad_W[I] = ti.Vector([
+            G_inv[0, 0] * A1_W[I] + G_inv[1, 0] * A2_W[I] + G_inv[2, 0] * A3_W[I],
+            G_inv[0, 1] * A1_W[I] + G_inv[1, 1] * A2_W[I] + G_inv[2, 1] * A3_W[I],
+            G_inv[0, 2] * A1_W[I] + G_inv[1, 2] * A2_W[I] + G_inv[2, 2] * A3_W[I]
+        ]) / cost[I]
 
 def get_boundary_conditions_SE2(source_point):
     """
@@ -376,7 +382,7 @@ def get_boundary_conditions_SE2(source_point):
     points and boundary values as TaiChi objects.
     """
     i_0, j_0, θ_0 = source_point
-    boundarypoints_np = np.array([[i_0, j_0, θ_0]], dtype=int)
+    boundarypoints_np = np.array([[i_0 + 1, j_0 + 1, θ_0 + 1]], dtype=int) # Account for padding.
     boundaryvalues_np = np.array([0.], dtype=float)
     boundarypoints = ti.Vector.field(n=3, dtype=ti.i32, shape=1)
     boundarypoints.from_numpy(boundarypoints_np)
