4b34d77292db64f3bea391beab8c809d25c1f438
Finn Sherry <f.m.sherry@tue.nl>
2024-01-30 11:55:18 +0100

    update python init files for new package structure.

--- eikivp/R2/__init__.py
+++ eikivp/R2/__init__.py
@@ -1,4 +1,6 @@
 # Access entire backend
+import eikivp.R2.distancemap
+import eikivp.R2.backtracking
 import eikivp.R2.derivatives
 import eikivp.R2.interpolate
 import eikivp.R2.vesselness
--- eikivp/__init__.py
+++ eikivp/__init__.py
@@ -1,11 +1,12 @@
 # Access entire backend
 import eikivp.utils
+import eikivp.cleanarrays
+import eikivp.costfunction
 import eikivp.R2
 import eikivp.SE2
 
-# # Most important functions are available at top level
-# ## R2
-# from iterativeeikonal.solvers import eikonal_solver_R2, geodesic_back_tracking_R2, convert_continuous_indices_to_real_space_R2
-# from iterativeeikonal.costfunctions import multiscale_frangi_filter_R2, cost_function
-# ## SE2
-# from iterativeeikonal.solvers import eikonal_solver_SE2_LI, geodesic_back_tracking_SE2, convert_continuous_indices_to_real_space_SE2
+# Most important functions are available at top level
+## R2
+from eikivp.R2.distancemap import eikonal_solver_R2
+from eikivp.R2.backtracking import geodesic_back_tracking_R2
+## SE2
