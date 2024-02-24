dd8afe114b630510ff95a386c5f3c4629ef36da9
Finn Sherry <f.m.sherry@tue.nl>
2024-02-24 17:01:46 +0100

    improve documentation for SE2 module.

--- eikivp/SE2/__init__.py
+++ eikivp/SE2/__init__.py
@@ -4,21 +4,20 @@
 
     Solve the Eikonal PDE on SE(2).
 
-    Provides the following "top level" submodules:
-      1. distancemap: compute the distance map (as solution of the Eikonal PDE)
-      with respect to some data-driven metric. This may be Riemannian, 
-      sub-Riemannian, or plus controller, so long as the metric is diagonal with
-      respect to the left invariant frame.
-      2. backtracking: compute the geodesic, with respect to the distance map,
-      connecting two points.
-      3. TODO vesselness: compute the SE(2) vesselness of an image, which can be
+    Contains three submodules for different controller types on SE(2), which
+    each contain methods for solving the corresponding Eikonal PDE and computing
+    geodesics:
+      1. Riemannian.
+      2. subRiemannian.
+      3. plus.
+
+    Moreover provides the following "top level" submodule:
+      1. TODO vesselness: compute the SE(2) vesselness of an image, which can be
       put into a cost function and subsequently into a data-driven metric. 
 
     Additionally, we have the following "internal" submodules
       1. derivatives: compute various derivatives of functions on SE(2).
-      2. interpolate: interpolate scalar and vector fields between grid points
-      with bilinear interpolation.
-      3. metric: compute the norm of vectors given some data-driven metric.
+      2. utils
 """
 
 # Access entire backend
